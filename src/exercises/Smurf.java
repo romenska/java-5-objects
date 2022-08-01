d447b1c758b6ab74dff28447784018eff8310880
Olha Romenska <romenska.olha@gmail.com>
2022-08-02 02:40:26 +0300

    only one papa smurf

--- src/exercises/Smurf.java
+++ src/exercises/Smurf.java
@@ -1,12 +1,23 @@
 package exercises;
 
+import java.util.*;
+
 public class Smurf {
 
 	private String name;
+	private static Map <String,Smurf> family = new HashMap();
 
 	public static Smurf createSmurf(String name) {
-		System.out.println("Creating " + name + " Smurf");
-		return new Smurf(name);
+		Smurf memberFamily ;
+		if (family.containsKey(name)) {
+			memberFamily =  family.get(name);
+		} else	{
+			
+			System.out.println("Creating " + name + " Smurf");
+			memberFamily =  new Smurf(name);
+			family.put(name, memberFamily);
+		}
+		return memberFamily;
 	}
 
 	private Smurf(String name) {
